e46c44efc740216132ab57df704613042e5d1a67
znhw <zenhew@yahoo.com>
2024-03-17 20:19:38 +0800

    edit urls for  get lessons markdown files

--- src/fetch/getPostMetadataF2.ts
+++ src/fetch/getPostMetadataF2.ts
@@ -5,14 +5,14 @@ import path from "path";
 
 // Get a list of content from folder with the file name
 const getPostMetadata = (): PostMetadata[] => {
-    const folder = "lessons/math-form-2/"
+    const folder = "/lessons/math-form-2/"
     const files = fs.readdirSync(folder)
     // const files = fs.readdirSync(path.join('./lessons/math-form-2'))
     const markdownPosts = files.filter((file) => file.endsWith(".md"))
   
     //Get gray-matter data from each file
     const lessons = markdownPosts.map((fileName) => {
-      const fileContents = fs.readFileSync(`lessons/math-form-2/${fileName}`, "utf8")
+      const fileContents = fs.readFileSync(`/lessons/math-form-2/${fileName}`, "utf8")
       const matterResult = matter(fileContents)
       return {
         title: matterResult.data.title,
