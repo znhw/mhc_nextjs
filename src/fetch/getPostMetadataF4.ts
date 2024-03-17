a0f7fbea1db7e0c0e495e72b5ac4b00d1965f821
znhw <zenhew@yahoo.com>
2024-03-18 00:37:21 +0800

    try again w '/' to lesson url in build dir

--- src/fetch/getPostMetadataF4.ts
+++ src/fetch/getPostMetadataF4.ts
@@ -5,13 +5,13 @@ import { PostMetadata } from './PostMetadata';
 
 // Get a list of content from folder with the file name
 const getPostMetadata = (): PostMetadata[] => {
-    const folder = "lessons/math-form-4/"
+    const folder = "/lessons/math-form-4/"
     const files = fs.readdirSync(folder)
     const markdownPosts = files.filter((file) => file.endsWith(".md"))
   
     //Get gray-matter data from each file
     const lessons = markdownPosts.map((fileName) => {
-      const fileContents = fs.readFileSync(`lessons/math-form-4/${fileName}`, "utf8")
+      const fileContents = fs.readFileSync(`/lessons/math-form-4/${fileName}`, "utf8")
       const matterResult = matter(fileContents)
       return {
         title: matterResult.data.title,
