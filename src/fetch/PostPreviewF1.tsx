7e777172419fd778c0522511d698982fa9714e54
znhw <zenhew@yahoo.com>
2024-03-18 13:04:19 +0800

    put back footer & socials plus change lesson list style same as home page

--- src/fetch/PostPreviewF1.tsx
+++ src/fetch/PostPreviewF1.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { PostMetadata } from "./PostMetadata";
+import styles from "../app/page.module.css";
 
 const PostPreview = ( props: PostMetadata) => {
     return <div key={props.slug}>
-        <Link href={`form-1/${props.slug}`}>
-        <h2>
+        <Link href={`form-1/${props.slug}`} className={styles.underlined}>
+        <p className={styles.underlined}> 
         {props.title}
-        </h2>
+        </p>
         {/* <p>{props.date}</p> */}
         </Link>     
     </div>
--- src/fetch/PostPreviewF5.tsx
+++ src/fetch/PostPreviewF5.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import { PostMetadata } from "./PostMetadata";
+import styles from "../app/page.module.css";
+
 
 const PostPreview = ( props: PostMetadata) => {
     return <div key={props.slug}>
         <Link href={`form-5/${props.slug}`}>
-        <h2>
+        <p className={styles.underlined}>
         {props.title}
-        </h2>
+        </p>
         </Link>     
     </div>
 }
